82557b528bc6af86454981869bd554603f06715b
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-02-26 21:56:03 -0300

    Implementação da funcionalidade de revelar a letra e alterar a imagem se errado

--- src/App.js
+++ src/App.js
@@ -12,33 +12,74 @@ export default function App(){
     const [choosenWord, setChoosenWord] = useState([])
     const [word, setWord] = useState([])
     // const [choosenLetter, setChoosenLetter] = useState([])
-
    
+    let numDashes = [];
+    let auxWrong=0;
+
     function startGame(){
-        sortWord()
-        setDisableInput(false)
-        setDisableLetter([])
+        sortWord();
+        setDisableInput(false);
+        setDisableLetter([]);
     }
 
     function sortWord(){
         const i = Math.floor(Math.random()*words.length)
-        const word = words[i]
-        console.log(word)
-        setChoosenWord(word.split(""))
-        const wordArray = word.split("")
+        const word = words[i];
+        console.log(word);
+        console.log(word.length);
+        setChoosenWord(word.split(""));
+        const wordArray = word.split("");
 
-        let numDashes = []
-        wordArray.forEach( () => numDashes.push("__ "))
-        setWord(numDashes)
+        // let numDashes = []
+        wordArray.forEach( () => numDashes.push("__ "));
+        setWord(numDashes);
+        console.log(numDashes.length);
     }
 
     function choosenLetter(letter){
-        // console.log(letter)
-        console.log(disableLetter)
-        const newDisableLetter=[... disableLetter, letter]
-        setDisableLetter(newDisableLetter)
-        console.log(disableLetter)
+        const newDisableLetter=[... disableLetter, letter];
+        setDisableLetter(newDisableLetter);
+        console.log(disableLetter);
+
+       if(choosenWord.includes(letter)){
+        showLetter(letter);
+       }
+       else{
+        wrongLetter();
+       } 
     }
+
+
+    function showLetter(letter){
+        // console.log(`input: ${letter}`)
+        // console.log(`Palavra: ${choosenWord[0]}`)
+        const newWord = [...word];
+        for (let i=0; i<word.length; i++){
+            if (letter===choosenWord[i]){
+                // console.log(`Valor do i: ${i}`)
+                // console.log(`Letra analisada: ${choosenWord[i]}`)
+                // console.log(`Acertou: a letra do input ${letter} confere com ${choosenWord[i]}`)
+                word[i]=choosenWord[i]
+            }
+            else {
+            }
+        }
+    }
+
+    function wrongLetter(){
+        const auxWrong = errors+1;
+        setErrors(auxWrong);
+        console.log(auxWrong);
+        
+        if(auxWrong==6){
+            loseGame()
+        }
+    }
+
+    function loseGame(auxWrong){
+        console.log("perdeu prayboy!")
+    }
+
 
     return(
         <div className="screen">
